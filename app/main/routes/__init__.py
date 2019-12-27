f170319c20d52445d00f240d25185cf8182a54b1
codehia <ach4825@gmail.com>
2019-12-27 18:34:01 +0530

    added logger

--- app/main/routes/__init__.py
+++ app/main/routes/__init__.py
@@ -1,7 +1,20 @@
-from .add_todo_restful import TodoSimple
-from flask_restful import Api
-
+# import resprctive blueprints and flask REstful resources
+from .blueprint_test import bp
 
 def add_resources(app):
-    api = Api(app)
-    api.add_resource(TodoSimple, '/')
+    """
+    Method to add resources to app context
+    
+    Args:
+        app (object): object of Flask representing the app in context
+    """
+    pass
+
+def register_blueprints(app):
+    """
+    Method to add blueprints to app context
+    
+    Args:
+        app (object): object of Flask representing the app in context
+    """
+    app.register_blueprint(bp)
